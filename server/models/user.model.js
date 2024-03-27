6b4b1a4621daf13b12ce48ed3dc6b04da39199c7
Rohit Jain <rohitjain9314@gmail.com>
2024-03-27 10:46:06 +0530

    Adding Google OAuth

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         requires: true,
     },
+    profilePicture: {
+        type: String,
+        default: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1711515470~exp=1711516070~hmac=9092e175abbd17f6b76d65e7cda221fcadd10e8732a73c7c44f38e9fd99eb037'
+    },
 }, { timestamps: true }
 );
 
